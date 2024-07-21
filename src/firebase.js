2f5746e048c2c63ec1d04c2d2c8687a46e744191
Anjali <anjalimaddheshiya99@gmail.com>
2024-07-21 12:29:22 +0530

    Added the home page and signin page

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBUQgGBzuJFeLjffPjhtKOpYhBzWkJ3Um4",
+    authDomain: "clone-7eed7.firebaseapp.com",
+    projectId: "clone-7eed7",
+    storageBucket: "clone-7eed7.appspot.com",
+    messagingSenderId: "20824593159",
+    appId: "1:20824593159:web:569781d33c9f9b4f1ceba2",
+    measurementId: "G-HF4877L5SW"
+  };
